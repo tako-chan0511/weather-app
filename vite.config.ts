0442826ed28bbee5077c2385cf246dc52776e5f9
Hara Keisuke <harakeisuke7@gmail.com>
2025-07-07 13:38:44 +0900

    PWA対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,42 @@
 // vite.config.ts
+import { fileURLToPath, URL } from 'node:url' //★
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa' //★
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+   VitePWA({
+      registerType: 'autoUpdate', // 更新があった場合に自動でリロードする
+      devOptions: {
+        enabled: true // 開発モードでもPWAの動作確認を可能にする
+      },
+      manifest: {
+        id: '/weather-app/',
+        name: '天気予報アプリ', // アプリのフルネーム
+        short_name: '天気アプリ', // ホーム画面に表示される短い名前
+        description: '住所、都市名、地図の位置の該当する天気を表示する。', // アプリの説明
+        start_url: '/weather-app/', // アプリ起動時のURL
+        display: 'standalone', // アドレスバーなどを表示しないネイティブアプリのような表示
+        background_color: '#ffffff', // スプラッシュ画面の背景色
+        theme_color: '#007acc',      // ツールバーの色
+        icons: [
+          {
+            src: 'icon-192x192.png', // publicディレクトリからの相対パス
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png', // publicディレクトリからの相対パス
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') },
   },
